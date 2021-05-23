e230d545204e93b7f3f21075fee0cd983dceb1f5
weixiaoqiang <1494129151@qq.com>
2021-05-23 17:08:11 +0800

    引入自定义样式

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import store from './store'
 // 引入element
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 引入自定义样式
+import 'assets/css/style.css'
 // 全局路由守卫
 import './router/permission.js'
 
