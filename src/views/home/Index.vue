613811fe13e20162fcc16fc7817b4e2e196250f5
weixiaoqiang <1494129151@qq.com>
2021-05-23 17:09:46 +0800

    新增首页

---
+++ src/views/home/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <span>欢迎来到后台管理系统</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
