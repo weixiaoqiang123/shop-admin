30b632b11dd0a78542d2f9ddb2ff528e1e9dbfd5
weixiaoqiang <1494129151@qq.com>
2021-05-31 16:14:24 +0800

    添加svg

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <svg :class="svgClass" aria-hidden="true" v-on="$listeners">
+    <use :xlink:href="iconName" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'SvgIcon',
+  props: {
+    iconClass: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    iconName() {
+      return `#icon-${this.iconClass}`
+    },
+    svgClass() {
+      if (this.className) {
+        return 'svg-icon ' + this.className
+      } else {
+        return 'svg-icon'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+svg{
+  margin-left: 5px;
+}
+
+.svg-icon {
+  width: 1.2em;
+  height: 1.2em;
+  vertical-align: -0.25em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = {
   publicPath: '/',
   outputDir: 'dist',
@@ -19,6 +24,19 @@ module.exports = {
         'model': '@/model'
       }
     }
+  },
+  chainWebpack(config) {
+      // 1.让svgloader排除掉src/icons这个目录
+      config.module.rule('svg')
+          .exclude.add(resolve('src/icons'));//以当前文件为起点 拼出src/icons的路径
+      
+      // 2.让插件svg-sprite-loader添加规则src/icons/svg
+      config.module.rule('icons')
+          .test(/\.svg$/)   //svg结尾的文件名
+          .include.add(resolve('src/icons')).end()//include数组操作 end回退到上一层
+          .use('svg-sprite-loader')
+              .loader('svg-sprite-loader')
+              .options({symbolId: 'icon-[name]'})
   },
   devServer: {
     // 启动端口
